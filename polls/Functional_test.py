6994722efb2555fbf20453b18922b92434606260
Alejandro Espinosa <alejo_sp@outlook.com>
2016-10-13 15:52:21 -0500

    paso 2: Rojo

--- polls/Functional_test.py
+++ polls/Functional_test.py
@@ -9,12 +9,51 @@ class BuscoAyudaTest(unittest.TestCase):
         self.browser = webdriver.Firefox()
 
     def  tearDown(self):
-        self.browser.quit()
-
+        #self.browser.quit()
+        pass
+        
     def test_title(self):
-        self.browser.get("127.0.0.1:8000")
+        self.browser.get("http://127.0.0.1:8000")
         self.assertIn('Busco Ayuda', self.browser.title)
 
+    def test_registro(self):
+        self.browser.get("http://127.0.0.1:8000")
+
+        register = self.browser.find_element_by_id('id_register')
+        register.click()
+
+        nombre = self.browser.find_element_by_id('id_nombre')
+        nombre.send_keys('Fulano')
+
+        apellido = self.browser.find_element_by_id('id_apellidos')
+        apellido.send_keys('Perez')
+
+        experiencia = self.browser.find_element_by_id('id_aniosExperiencia')
+        experiencia.send_keys('2')
+
+        self.browser.find_element_by_xpath("//select[id='id_tiposDeServicio']/option[text()='Desarrollador Web']").click()
+
+        telefono = self.browser.find_element_by_id('id_telefono')
+        telefono.send_keys('37465827475')
+
+        correo = self.browser.find_element_by_id('id_correo')
+        correo.send_keys('qwe@qwe.qwe')
+
+        imagen = self.browser.find_element_by_id('id_imagen')
+        imagen.send_keys('C:\\Users\\alejo\\Google Drive\\IFTTT\\Spotify\\Orochi by Orochi.jpg')
+
+        nombreUsuario = self.browser.find_element_by_id('id_username')
+        nombreUsuario.send_keys('NoEsOtroFulano')
+
+        clave = self.browser.find_element_by_id('id_password')
+        clave.send_keys('clave123')
+
+        boton_grabar = self.browser.find_element_by_id('id_grabar')
+        boton_grabar.click()
+
+        self.browser.implicitly_wait(3)
+        span = self.browser.find_element(By.XPATH, '//span[text()=[Fulano Perez]]')
+        self.assertIn('Fulano Perez', span.text)
 
 if __name__ == '__main__':
     unittest.main()
